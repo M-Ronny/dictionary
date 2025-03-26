3ee05406e9edde175577306afda46d69a9d1397e
Ronaldo <ronaldomorales.ai@gmail.com>
2025-03-26 15:01:25 -0400

    updated index.html and added innertext on classes on main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ function getDefinition() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      console.log(data[0].meanings)
+      document.querySelector('.word').innerText = data[0].word
+      document.querySelector('.partOfSpeech').innerText = data[0].meanings[0].partOfSpeech
+      document.querySelector('.definition').innerText = data[0].meanings[0].definitions[0].definition
+      document.querySelector('.synomyms').innerText = data[0].meanings[0].synonyms.join(', ')
     })
     .catch(err => {
         console.log(`error ${err}`)
