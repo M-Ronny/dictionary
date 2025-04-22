283883a97341d03e1e1a673ab67b03c4b854b644
Ronaldo <ronaldomorales.ai@gmail.com>
2025-04-22 16:29:38 -0400

    updated index.html and main.js

--- js/main.js
+++ js/main.js
@@ -7,27 +7,6 @@ function getDefinition() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      // document.querySelector('.word').innerText = data[0].word
-      // document.querySelector('.partOfSpeech').innerText = data[0].meanings[0].partOfSpeech
-      // document.querySelector('.synomyms').innerText = data[0].meanings[0].synonyms.join(', ')
-      // document.querySelector('.definition').textContent = ''
-      
-      // data[0].meanings[0].definitions.forEach(obj => {
-      //   const li = document.createElement('li')
-      //   li.textContent = obj.definition
-      //   document.querySelector('.definition').appendChild(li)
-      // })
-
-      // document.querySelector('.wordTwo').innerText = data[0].word
-      // document.querySelector('.speechTwo').innerText = data[0].meanings[1].partOfSpeech
-      // document.querySelector('.symTwo').innerText = data[0].meanings[1].synonyms.join(', ')
-      // document.querySelector('.defTwo').textContent = ''
-      
-      // data[0].meanings[1].definitions.forEach(obj => {
-      //   const li = document.createElement('li')
-      //   li.textContent = obj.definition
-      //   document.querySelector('.defTwo').appendChild(li)
-      // })
       document.querySelector('.getDefinition').textContent = ''
 
       data[0].meanings.forEach(obj => {
